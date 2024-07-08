5c9d62d4045dfbb9d45c91d39ec5399fc74da070
rohit chavhan <rohitchavhan46@gmail.com>
2024-07-08 18:34:51 +0530

    refactored the code used ternary operator in place of if statements

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+// Query Selectors
 let addBooksBtn = document.querySelector('.addBooks');
 let submitBtn = document.querySelector('.submit');
 const cardsMainDiv = document.querySelector('.gridCards');
 const dialog = document.querySelector('dialog');
 
+// Book Class
 class Book {
   constructor(bookName, author, pages, status) {
     this.bookName = bookName;
@@ -10,27 +12,27 @@ class Book {
     this.pages = pages;
     this.status = status;
   }
-  changeStatus() {
-    if (this.status === 'Read') {
-      this.status = 'Not Read';
-    } else {
-      this.status = 'Read';
-    }
+  toggleStatus() {
+    this.status = this.status === 'Read' ? 'Not Read' : 'Read';
   }
 }
 
+// Library Array
 const myLibrary = [];
 
+// Dialog Event Listener
 dialog.addEventListener('click', (event) => {
   if (event.target.id === 'form') {
     dialog.close();
   }
 });
 
+// Add Books Button Event Listener
 addBooksBtn.addEventListener('click', () => {
   dialog.showModal();
 });
 
+// Submit Button Event Listener
 submitBtn.addEventListener('click', (event) => {
   getValues();
   if (myLibrary.length > 1) {
@@ -41,35 +43,34 @@ submitBtn.addEventListener('click', (event) => {
   event.preventDefault();
 });
 
+// Function to Add Book to Library
 function getValues() {
   const bookName = document.getElementById('bookName').value;
   const numsOfPages = document.getElementById('numsOfPages').value;
   const author = document.getElementById('author').value;
-  const status = document.getElementById('status');
+  const status = document.getElementById('status').checked
+    ? 'Read'
+    : 'Not read';
 
-  let readOrNot = '';
-  if (status.checked === true) {
-    readOrNot = 'Read';
-  } else {
-    readOrNot = 'Not read';
-  }
-
-  const book = new Book(bookName, author, numsOfPages, readOrNot);
+  const book = new Book(bookName, author, numsOfPages, status);
   myLibrary.push(book);
+}
+
+function domInAction() {
+  removeCards();
+  createCards();
 }
 
 function getAttribute(elm) {
   const arrayIndex = Number(elm.getAttribute('index'));
-  myLibrary[arrayIndex].changeStatus();
-  removeCards();
-  createCards();
+  myLibrary[arrayIndex].toggleStatus();
+  domInAction();
 }
 
 function removeArrayElm(elm) {
   const index = Number(elm.getAttribute('removeIndex'));
   myLibrary.splice(index, 1);
-  removeCards();
-  createCards();
+  domInAction();
 }
 
 function createCards() {
@@ -88,13 +89,8 @@ function createCards() {
     status.innerHTML = `${el.status}`;
 
     status.classList.add('btn');
-    removeBtn.classList.add('btn');
-    removeBtn.classList.add('width100');
-    if (status.innerHTML === 'Read') {
-      status.classList.add('green-btn');
-    } else {
-      status.classList.add('red-btn');
-    }
+    removeBtn.classList.add('btn', 'width100');
+    status.classList.add(status.innerHTML === 'Read' ? 'green-btn' : 'red-btn');
 
     status.setAttribute('index', `${i}`);
     removeBtn.setAttribute('removeIndex', `${i}`);
@@ -107,13 +103,8 @@ function createCards() {
       removeArrayElm(this);
     });
 
-    div.appendChild(authorN);
-    div.appendChild(bookN);
-    div.appendChild(bookP);
-    div.appendChild(status);
     div.classList.add('cards');
-    div.appendChild(removeBtn);
-
+    div.append(authorN, bookN, bookP, status, removeBtn);
     cardsMainDiv.appendChild(div);
   });
 }
